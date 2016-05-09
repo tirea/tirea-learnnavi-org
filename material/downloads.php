a810f0bcbdeaf68f9c15a093c3d0f9acbcd10432
tirea <tirea@learnnavi.org>
2016-05-09 03:48:06 -0700

    card-panel links on downloads page

--- material/downloads.php
+++ material/downloads.php
@@ -1,112 +1,180 @@
 ﻿<?php include "res/themetop.php" ?>
-      <span class="titlename">
-        Downloads
-      </span>
-      <h4>Lessons as PDF</h4>
-      <div class="tabcontd">
-        <a href="http://tirea.learnnavi.org/download/1.pdf">
-          <div class="imgd">
-            <div class="descd">1. Order, -l &amp; -t</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/2.pdf">
-          <div class="imgd">
-            <div class="descd">2. Past and Future</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/3.pdf">
-          <div class="imgd">
-            <div class="descd">3. Have, Plurals, Changes</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/4.pdf">
-          <div class="imgd">
-            <div class="descd">4. Describing Stuff</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/5.pdf">
-          <div class="imgd">
-            <div class="descd">5. Questions</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/6.pdf">
-          <div class="imgd">
-            <div class="descd">6. This &amp; That +misc</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/7.pdf">
-          <div class="imgd">
-            <div class="descd">7. -ru, -yä, and si</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/8.pdf">
-          <div class="imgd">
-            <div class="descd">8. &lt;iv&gt;</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/9.pdf">
-          <div class="imgd">
-            <div class="descd">9. -ri</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/10.pdf">
-          <div class="imgd">
-            <div class="descd">10. No and Not</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/11.pdf">
-          <div class="imgd">
-            <div class="descd">11. Pum</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/12.pdf">
-          <div class="imgd">
-            <div class="descd">12. -ing in Na&#39;vi</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/13.pdf">
-          <div class="imgd">
-            <div class="descd">13. F-words</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/14.pdf">
-          <div class="imgd">
-            <div class="descd">14. Colours</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/15.pdf">
-          <div class="imgd">
-            <div class="descd">15. Numbers</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/16.pdf">
-          <div class="imgd">
-            <div class="descd">16. &lt;eyk&gt;</div>
-          </div>
-        </a>
-        <a href="http://tirea.learnnavi.org/download/17.pdf">
-          <div class="imgd">
-            <div class="descd">17. Self</div>
-          </div>
-        </a>
+<span class="titlename">
+Downloads
+</span>
+<h4>Lessons as PDF</h4>
+<div class="row">
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/1.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        1. Order, -l &amp; -t
+        </span>
       </div>
-      <h4>Other Things Hosted On This Site</h4>
-      <ul class="nobullets">
-        <li>
-          <a href="http://tirea.learnnavi.org/download/">download folder</a> | Some useful stuff you might want to look at or have
-        </li>
-        <li>
-          <a href="http://tirea.learnnavi.org/download/hkb-kxwerty-mod.apk">hkb-kxwerty-mod.apk</a> | Hacker&#39;s Keyboard Mod (KXWERTY for Android)
-        </li>
-        <li>
-          <a href="http://tirea.learnnavi.org/download/navkb6.zip">navkb6.zip</a> | KXWERTY for Windows / Mac
-        </li>
-        <li>
-          <a href="http://tirea.learnnavi.org/generate.php">generate.php</a> | Na&#39;vi Name Generator (An actually good one)
-        </li>
-        <li>
-          <a href="http://tirea.learnnavi.org/images">images folder</a> | Bunch of old memes and comics and whatnot, some Na&#39;vi-related
-        </li>
-      </ul>
-    </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/2.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        2. Past and Future
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/3.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        3. Have, Plurals, Changes
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/4.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        4. Describing Stuff
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/5.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        5. Questions
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/6.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        6. This &amp; That +misc
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/7.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        7. -ru, -yä, and si
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/8.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        8. &lt;iv&gt;
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/9.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        9. -ri
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/10.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        10. No and Not
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/11.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        11. Pum
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/12.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        12. -ing in Na&#39;vi
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/13.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        13. F-words
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/14.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        14. Colours
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/15.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        15. Numbers
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/16.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        16. &lt;eyk&gt;
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="http://tirea.learnnavi.org/download/17.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        17. Self
+        </span>
+      </div>
+    </a>
+  </div>
+</div>
+<h4>Other Things Hosted On This Site</h4>
+<ul class="nobullets">
+  <li>
+    <a href="http://tirea.learnnavi.org/download/">download folder</a> | Some useful stuff you might want to look at or have
+  </li>
+  <li>
+    <a href="http://tirea.learnnavi.org/download/hkb-kxwerty-mod.apk">hkb-kxwerty-mod.apk</a> | Hacker&#39;s Keyboard Mod (KXWERTY for Android)
+  </li>
+  <li>
+    <a href="http://tirea.learnnavi.org/download/navkb6.zip">navkb6.zip</a> | KXWERTY for Windows / Mac
+  </li>
+  <li>
+    <a href="http://tirea.learnnavi.org/generate.php">generate.php</a> | Na&#39;vi Name Generator (An actually good one)
+  </li>
+  <li>
+    <a href="http://tirea.learnnavi.org/images">images folder</a> | Bunch of old memes and comics and whatnot, some Na&#39;vi-related
+  </li>
+</ul>
+</div>
 <?php include "res/themebottom.php" ?>
