6f5f62b153307794e72740ab515950700f402714
tirea <tirea@learnnavi.org>
2016-05-22 14:21:24 -0700

    formatting and such, add little more info

---
+++ material/n1.php
@@ -0,0 +1,9 @@
+<?php include "res/themetop.php" ?>
+
+      <div class="titlename">Pronouns (pn.)</div>
+
+      <p>Pronouns are words like I, you, he, she, it, 
+        they, etc. Na'vi Pronouns are specific but 
+        simple and logical.</p>
+
+<?php include "res/themebottom.php" ?>
--- material/nouns.php
+++ material/nouns.php
@@ -1,7 +1,27 @@
 <?php include "res/themetop.php" ?>
 
-<div class="titlename">Nouns</div>
+      <div class="titlename">Nouns (n.)</div>
 
-<p>Coming Soon</p>
+      <p>Nouns are persons, places, things. 
+        Na?vi nouns have no grammatical gender 
+        and no ?the? or ?a?. This way, you can learn 
+        the word and use it. Sometimes it changes. 
+        (more about that later.) To learn more about
+        how nouns work, check out the selections
+        below.</p>
+
+      <h4>Lessons</h4>
+
+      <div class="row">
+        <div class="col s12 m3">
+          <a href="n1.php">
+            <div class="card-panel cyan darken-2">
+              <span class="white-text">
+                1. Pronouns (pn.)
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
 
 <?php include "res/themebottom.php" ?>
