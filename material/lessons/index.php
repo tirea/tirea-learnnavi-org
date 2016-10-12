c0dcedcdb9c6a47b0abaff8b445f2f3ec801932e
Tirea Aean <tirea@learnnavi.org>
2016-10-11 18:04:23 -0700

    delete blank lessons

--- material/lessons/index.php
+++ material/lessons/index.php
@@ -6,9 +6,6 @@
         <ul class="collection with-header">
           <li class="collection-header"><h4>Beginners' Basics</h4></li>
           <li class="collection-item"><a class="collection-link" href="01.php">Greetings &amp; Introductions</a></li>
-          <li class="collection-item"><a cless="collection-link" href="02.php">Having and Giving</a></li>
-          <li class="collection-item"><a class="collection-link" href="03.php">Asking Questions</a></li>
-          <li class="collection-item"><a class="collection-link" href="04.php">Talking about Weather</a></li>
         </ul>
 
       </div>
