56e82841bb8e2959370afa02fac81e932f658c29
Tirea Aean <tirea@learnnavi.org>
2016-10-11 19:36:03 -0700

    Rename rss.xml to feed.rss

--- material/res/themetop.php
+++ material/res/themetop.php
@@ -20,7 +20,7 @@
           <li><a href="/material/lessons/index.php">Lessons</a></li>
           <li><a href="/material/links.php">Links</a></li>
           <li><a href="/material/download/index.php">Downloads</a></li>
-          <li id="rss-nav-item"><a id="rss-link" href="/material/rss.xml"><img id="rss-icon" src="/material/res/rss-icon.png"></a></li>
+          <li id="rss-nav-item"><a id="rss-link" href="/material/feed.rss"><img id="rss-icon" src="/material/res/rss-icon.png"></a></li>
         </ul>
         <ul class="side-nav" id="mobilenav">
           <li><a href="/material/index.php">Home</a></li>
@@ -28,7 +28,7 @@
           <li><a href="/material/lessons/index.php">Lessons</a></li>
           <li><a href="/material/links.php">Links</a></li>
           <li><a href="/material/download/index.php">Downloads</a></li>
-          <li><a href="/material/rss.xml">RSS Feed</a></li>
+          <li><a href="/material/feed.rss">RSS Feed</a></li>
         </ul>
       </div>
     </nav>
