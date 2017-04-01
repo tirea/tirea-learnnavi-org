739a00897ae9f24060e3d1debc60e1723d6c7294
Vawmataw <fisenrique@gmail.com>
2017-04-01 10:44:38 -0400

    Update sounds.php
    Last fix0ring

--- material/sounds.php
+++ material/sounds.php
@@ -67,7 +67,7 @@
   </ul>
 
   <ul class="collection with-header">
-    <li class="collection-header"><h4>Ep'anga pam lepay</h4></li>
+    <li class="collection-header"><h4>Ep'anga fam lepay</h4></li>
     <li class="collection-item">
       <div>aw [aw]
         <a id="play_aw" class="secondary-content amber-text">
@@ -99,7 +99,7 @@
   </ul>
 
   <ul class="collection with-header">
-    <li class="collection-header"><h4>Pam a fmi livu lepay</h4></li>
+    <li class="collection-header"><h4>Mefam a fmi livu lepay</h4></li>
     <li class="collection-item">
       <div>ll [ḷ]
         <a id="play_ll" class="secondary-content amber-text">
